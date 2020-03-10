f032983b880ce46a1c5d84bd23b36aed2a3f7ade
Darius Clop <clop_darius@yahoo.com>
2020-03-11 00:03:59 +0200

    Add navigation on dashboard

--- app/src/main/java/com/example/smartparentalapp/MainActivity.java
+++ app/src/main/java/com/example/smartparentalapp/MainActivity.java
@@ -2,15 +2,43 @@ package com.example.smartparentalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView clickedMenuItem = findViewById(R.id.bottom_navigation);
+        clickedMenuItem.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        public boolean onNavigationItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.dashboardPage:
+                    Intent dashboardIntent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivityForResult(dashboardIntent, 0);
+                    return true;
+                case R.id.locationPage:
+                    Intent locationIntent = new Intent(getApplicationContext(), LocationActivity.class);
+                    startActivityForResult(locationIntent, 0);
+                    return true;
+                case R.id.loginPage:
+                    Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivityForResult(loginIntent, 0);
+                    return true;
+
+            }
+            return true;
+        }
+    };
 }
