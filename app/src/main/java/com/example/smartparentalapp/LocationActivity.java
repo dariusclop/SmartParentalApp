d6cd5314f0629eac37f40bd22da12053cf1ada30
Darius Clop <clop_darius@yahoo.com>
2020-03-14 19:40:32 +0200

    Add last known location functionality for location page

--- app/src/main/java/com/example/smartparentalapp/LocationActivity.java
+++ app/src/main/java/com/example/smartparentalapp/LocationActivity.java
@@ -1,22 +1,39 @@
 package com.example.smartparentalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.location.Location;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Locale;
+
 public class LocationActivity extends AppCompatActivity {
     private FirebaseAuth dbAuth;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         dbAuth = FirebaseAuth.getInstance();
         FirebaseUser userSignedIn = dbAuth.getCurrentUser();
+
+        //Location
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
         getSupportActionBar().hide();
@@ -31,6 +48,23 @@ public class LocationActivity extends AppCompatActivity {
             clickedMenuItem.getMenu().removeItem(R.id.profilePage);
         }
         clickedMenuItem.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+    }
+
+    private void getLocation() {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location.equals(currentLocation))
+                        {
+                            Toast.makeText(getApplicationContext(), "Location fetching failed. Make sure to have location activated", Toast.LENGTH_LONG).show();
+                        }
+                        if (location != null) {
+                            currentLocation = location;
+                            Toast.makeText(getApplicationContext(), "Location fetching success", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     //Menu click listener
