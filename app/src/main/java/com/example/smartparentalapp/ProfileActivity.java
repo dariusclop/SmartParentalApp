eafd491a6c2e257d05395e4a7d1f9eb0591b21c1
Darius Clop <clop_darius@yahoo.com>
2020-03-14 15:54:27 +0200

    Add logout functionality to user

--- app/src/main/java/com/example/smartparentalapp/ProfileActivity.java
+++ app/src/main/java/com/example/smartparentalapp/ProfileActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +25,9 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         getSupportActionBar().hide();
 
+        // Buttons
+        Button logoutButton = (Button) findViewById(R.id.logoutButton);
+
         //Menu set click listener
         BottomNavigationView clickedMenuItem = findViewById(R.id.bottom_navigation);
         if(userSignedIn != null) {
@@ -31,6 +38,28 @@ public class ProfileActivity extends AppCompatActivity {
             clickedMenuItem.getMenu().removeItem(R.id.profilePage);
         }
         clickedMenuItem.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        //Button click listener
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               signOut();
+            }
+        });
+    }
+
+    private void signOut() {
+        dbAuth.signOut();
+        updateUI(null);
+    }
+
+    public void updateUI(FirebaseUser account){
+        if(account != null){
+            Toast.makeText(this,"Log out failed",Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(this,"Logged out",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this,  MainActivity.class));
+        }
     }
 
     //Menu click listener
