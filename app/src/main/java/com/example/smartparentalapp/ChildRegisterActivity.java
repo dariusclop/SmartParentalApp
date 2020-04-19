a18929dda005b024a031591e685374fac095b457
Darius Clop <clop_darius@yahoo.com>
2020-04-19 21:48:17 +0300

    Add validation for fields on parent/child register forms

--- app/src/main/java/com/example/smartparentalapp/ChildRegisterActivity.java
+++ app/src/main/java/com/example/smartparentalapp/ChildRegisterActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,6 +17,12 @@ import com.google.firebase.auth.FirebaseUser;
 public class ChildRegisterActivity extends AppCompatActivity {
     private FirebaseAuth dbAuth;
     private MenuHelper menuHelper;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private EditText mConfirmPasswordField;
+    private EditText mDisplayNameField;
+    private EditText mGeneratedCodeField;
+    private boolean isRegistrationValid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +42,55 @@ public class ChildRegisterActivity extends AppCompatActivity {
             clickedMenuItem.getMenu().removeItem(R.id.profilePage);
         }
         clickedMenuItem.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        // Views
+        mEmailField = findViewById(R.id.emailChild);
+        mPasswordField = findViewById(R.id.passwordChild);
+        mConfirmPasswordField = findViewById(R.id.confirmPasswordChild);
+        mDisplayNameField = findViewById(R.id.displayChildName);
+        mGeneratedCodeField = findViewById(R.id.generatedCode);
+
+        // Buttons
+        Button registerChildButton = findViewById(R.id.registerChildButton);
+
+        //Button click listener
+        registerChildButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRegistrationValid = true;
+
+                if(TextUtils.isEmpty(mEmailField.getText().toString())) {
+                    isRegistrationValid = false;
+                    mEmailField.setError("Field cannot be empty");
+                }
+                if(TextUtils.isEmpty(mPasswordField.getText().toString())) {
+                    isRegistrationValid = false;
+                    mPasswordField.setError("Field cannot be empty");
+                }
+                if(TextUtils.isEmpty(mConfirmPasswordField.getText().toString())) {
+                    isRegistrationValid = false;
+                    mConfirmPasswordField.setError("Field cannot be empty");
+                }
+                if(TextUtils.isEmpty(mDisplayNameField.getText().toString())) {
+                    isRegistrationValid = false;
+                    mDisplayNameField.setError("Field cannot be empty");
+                }
+                if(TextUtils.isEmpty(mGeneratedCodeField.getText().toString())) {
+                    isRegistrationValid = false;
+                    mGeneratedCodeField.setError("Field cannot be empty");
+                }
+                if(TextUtils.isDigitsOnly(mPasswordField.getText().toString()))
+                {
+                    isRegistrationValid = false;
+                    mPasswordField.setError("Password can't be only digits");
+                }
+                if(!TextUtils.equals(mPasswordField.getText().toString(), mConfirmPasswordField.getText().toString()))
+                {
+                    isRegistrationValid = false;
+                    mConfirmPasswordField.setError("Passwords don't match");
+                }
+            }
+        });
     }
 
     //Menu click listener
--- app/src/main/java/com/example/smartparentalapp/ParentRegisterActivity.java
+++ app/src/main/java/com/example/smartparentalapp/ParentRegisterActivity.java
@@ -4,15 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.w3c.dom.Text;
+
 public class ParentRegisterActivity extends AppCompatActivity {
     private FirebaseAuth dbAuth;
     private MenuHelper menuHelper;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private EditText mConfirmPasswordField;
+    private EditText mDisplayNameField;
+    private boolean isRegistrationValid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,50 @@ public class ParentRegisterActivity extends AppCompatActivity {
             clickedMenuItem.getMenu().removeItem(R.id.profilePage);
         }
         clickedMenuItem.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        // Views
+        mEmailField = findViewById(R.id.emailParent);
+        mPasswordField = findViewById(R.id.passwordParent);
+        mConfirmPasswordField = findViewById(R.id.confirmPasswordParent);
+        mDisplayNameField = findViewById(R.id.displayParentName);
+
+        // Buttons
+        Button registerParentButton = findViewById(R.id.registerParentButton);
+
+        //Button click listener
+        registerParentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRegistrationValid = true;
+
+                if(TextUtils.isEmpty(mEmailField.getText().toString())) {
+                    isRegistrationValid = false;
+                    mEmailField.setError("Field cannot be empty");
+                }
+                if(TextUtils.isEmpty(mPasswordField.getText().toString())) {
+                    isRegistrationValid = false;
+                    mPasswordField.setError("Field cannot be empty");
+                }
+                if(TextUtils.isEmpty(mConfirmPasswordField.getText().toString())) {
+                    isRegistrationValid = false;
+                    mConfirmPasswordField.setError("Field cannot be empty");
+                }
+                if(TextUtils.isEmpty(mDisplayNameField.getText().toString())) {
+                    isRegistrationValid = false;
+                    mDisplayNameField.setError("Field cannot be empty");
+                }
+                if(TextUtils.isDigitsOnly(mPasswordField.getText().toString()))
+                {
+                    isRegistrationValid = false;
+                    mPasswordField.setError("Password can't be only digits");
+                }
+                if(!TextUtils.equals(mPasswordField.getText().toString(), mConfirmPasswordField.getText().toString()))
+                {
+                    isRegistrationValid = false;
+                    mConfirmPasswordField.setError("Passwords don't match");
+                }
+            }
+        });
     }
 
     //Menu click listener
